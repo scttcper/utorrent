f8db5d729224b4cba1f0bf6e2b29ffa79f68b265
Scott Cooper <scttcper@gmail.com>
2022-02-27 12:56:52 -0800

    feat: convert project to esm, use got v12 (#61)
    BREAKING CHANGE: upgrade got to v12
    BREAKING CHANGE: This package is now pure ESM. https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c
    BREAKING CHANGE: requires node > 14.16
    
    - adds @ctrl/magnet-link as a dependency
    - adds support for adding torrent magnet links to normalizedAddTorrent
    - new function `addTorrentFromUrl`

---
+++ src/utorrent.ts
@@ -0,0 +1,496 @@
+import { existsSync } from 'fs';
+import { URLSearchParams } from 'url';
+
+import { File, FormData } from 'formdata-node';
+import { fileFromPathSync } from 'formdata-node/file-from-path';
+import got, { OptionsOfTextResponseBody, Response } from 'got';
+import { Cookie } from 'tough-cookie';
+
+import { magnetDecode } from '@ctrl/magnet-link';
+import {
+  AddTorrentOptions as NormalizedAddTorrentOptions,
+  AllClientData,
+  NormalizedTorrent,
+  TorrentClient,
+  TorrentSettings,
+  TorrentState,
+} from '@ctrl/shared-torrent';
+import { hash } from '@ctrl/torrent-file';
+import { urlJoin } from '@ctrl/url-join';
+
+import {
+  BaseResponse,
+  RssUpdateResponse,
+  SettingsResponse,
+  TorrentData,
+  TorrentListResponse,
+  VersionResponse,
+} from './types.js';
+
+const defaults: TorrentSettings = {
+  baseUrl: 'http://localhost:44822/',
+  path: '/gui/',
+  username: 'admin',
+  password: '',
+  timeout: 5000,
+};
+
+export class Utorrent implements TorrentClient {
+  config: TorrentSettings;
+
+  private _token?: string;
+
+  private _cookie?: Cookie;
+
+  constructor(options: Partial<TorrentSettings> = {}) {
+    this.config = { ...defaults, ...options };
+  }
+
+  resetSession(): void {
+    this._token = undefined;
+    this._cookie = undefined;
+  }
+
+  async getSettings(): Promise<SettingsResponse> {
+    const res = await this.request<SettingsResponse>('getsettings');
+    return res.body;
+  }
+
+  async getVersion(): Promise<VersionResponse> {
+    const res = await this.request<VersionResponse>('getversion');
+    return res.body;
+  }
+
+  /**
+   * alias of unpause, resumes a torrent
+   */
+  async resumeTorrent(hash: string): Promise<BaseResponse> {
+    return this.unpause(hash);
+  }
+
+  async unpause(hash: string): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.set('hash', hash);
+    const res = await this.request<BaseResponse>('unpause', params);
+    return res.body;
+  }
+
+  async forceStartTorrent(hash: string): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.set('hash', hash);
+    const res = await this.request<BaseResponse>('forcestart', params);
+    return res.body;
+  }
+
+  async pauseTorrent(hash: string): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.set('hash', hash);
+    const res = await this.request<BaseResponse>('pause', params);
+    return res.body;
+  }
+
+  async stopTorrent(hash: string): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.set('hash', hash);
+    const res = await this.request<BaseResponse>('stop', params);
+    return res.body;
+  }
+
+  async queueUp(hash: string): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.set('hash', hash);
+    const res = await this.request<BaseResponse>('queueup', params);
+    return res.body;
+  }
+
+  async queueDown(hash: string): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.set('hash', hash);
+    const res = await this.request<BaseResponse>('queuedown', params);
+    return res.body;
+  }
+
+  async queueTop(hash: string): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.set('hash', hash);
+    const res = await this.request<BaseResponse>('queuetop', params);
+    return res.body;
+  }
+
+  async queueBottom(hash: string): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.set('hash', hash);
+    const res = await this.request<BaseResponse>('queuebottom', params);
+    return res.body;
+  }
+
+  async removeTorrent(hash: string, removeData = true): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.set('hash', hash);
+
+    // decide action from remove torrent data
+    let action = 'removetorrent';
+    if (removeData) {
+      action = 'removedatatorrent';
+    }
+
+    const res = await this.request<BaseResponse>(action, params);
+    return res.body;
+  }
+
+  async setProps(hash: string, props: Record<string, string | number>): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    for (const prop of Object.entries(props)) {
+      params.set(prop[0], prop[1].toString());
+    }
+
+    params.set('hash', hash);
+
+    const res = await this.request<BaseResponse>('setprops', params);
+    return res.body;
+  }
+
+  async normalizedAddTorrent(
+    torrent: string | Buffer,
+    options: Partial<NormalizedAddTorrentOptions> = {},
+  ): Promise<NormalizedTorrent> {
+    let torrentHash: string | undefined;
+    if (typeof torrent === 'string' && torrent.startsWith('magnet:')) {
+      torrentHash = magnetDecode(torrent).infoHash;
+      if (!torrentHash) {
+        throw new Error('Magnet did not contain hash');
+      }
+
+      await this.addTorrentFromUrl(torrent);
+    } else {
+      if (!Buffer.isBuffer(torrent)) {
+        torrent = Buffer.from(torrent);
+      }
+
+      torrentHash = await hash(torrent);
+      await this.addTorrent(torrent);
+    }
+
+    if (options.startPaused) {
+      await this.pauseTorrent(torrentHash);
+    }
+
+    if (options.label) {
+      await this.setProps(torrentHash, {
+        s: 'label',
+        v: options.label,
+      });
+    }
+
+    return this.getTorrent(torrentHash);
+  }
+
+  async getTorrent(id: string): Promise<NormalizedTorrent> {
+    const listResponse = await this.listTorrents();
+    const torrentData = listResponse.torrents.find(n => n[0].toLowerCase() === id.toLowerCase());
+    if (!torrentData) {
+      throw new Error('Torrent not found');
+    }
+
+    return this._normalizeTorrentData(torrentData);
+  }
+
+  async getAllData(): Promise<AllClientData> {
+    const listTorrents = await this.listTorrents();
+    const results: AllClientData = {
+      torrents: [],
+      labels: [],
+    };
+
+    for (const torrent of listTorrents.torrents) {
+      const torrentData: NormalizedTorrent = this._normalizeTorrentData(torrent);
+      results.torrents.push(torrentData);
+    }
+
+    for (const label of listTorrents.label) {
+      results.labels.push({
+        id: label[0],
+        name: label[0],
+        count: label[1],
+      });
+    }
+
+    return results;
+  }
+
+  async addTorrent(torrent: string | Buffer): Promise<BaseResponse> {
+    if (this._cookie) {
+      // eslint-disable-next-line new-cap
+      if (this._cookie.TTL() < 5000) {
+        this.resetSession();
+      }
+    }
+
+    if (!this._token) {
+      await this.connect();
+    }
+
+    const form = new FormData();
+    const type = { type: 'application/x-bittorrent' };
+    if (typeof torrent === 'string') {
+      if (existsSync(torrent)) {
+        form.set('torrent_file', fileFromPathSync(torrent, 'file.torrent', type));
+      } else {
+        form.set('torrent_file', new File([Buffer.from(torrent, 'base64')], 'file.torrent', type));
+      }
+    } else {
+      const file = new File([torrent], 'file.torrent', type);
+      form.set('torrent_file', file);
+    }
+
+    const params = new URLSearchParams();
+    params.set('download_dir', '0');
+    params.set('path', '');
+    params.set('action', 'add-file');
+    params.set('token', this._token!);
+
+    const url = urlJoin(this.config.baseUrl, this.config.path);
+
+    const res = await got
+      .post(url, {
+        headers: {
+          Authorization: this._authorization(),
+          Cookie: this._cookie?.cookieString(),
+        },
+        searchParams: params,
+        body: form,
+        retry: { limit: 0 },
+        timeout: { request: this.config.timeout },
+        ...(this.config.agent ? { agent: this.config.agent } : {}),
+      })
+      .json<BaseResponse>();
+
+    return res;
+  }
+
+  /**
+   * Add torrent from url, probably a magnet
+   */
+  async addTorrentFromUrl(urlOrMagnet: string): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.append('s', urlOrMagnet);
+    const res = await this.request<BaseResponse>('add-url', params);
+    return res.body;
+  }
+
+  /**
+   * set a setting
+   * @param settings settings to set [setting_name, value] as array of key value tuples
+   */
+  async setSetting(settings: Array<[string, string | number]>): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    for (const setting of settings) {
+      params.append('s', setting[0]);
+      params.append('v', setting[1].toString());
+    }
+
+    const res = await this.request<VersionResponse>('setsetting', params);
+    return res.body;
+  }
+
+  /**
+   * subscribe to rss feed
+   * @param url feed url
+   * @param [id] id of rss feed to update, -1 for new feed
+   * @param [alias] custom alias
+   * @param [subscribe] Automatically download all items published in feed
+   * @param [smartFilter] Use smart episode filter
+   * @param [enabled] disable / enable an rss feed
+   */
+  // eslint-disable-next-line max-params
+  async rssUpdate(
+    url: string,
+    id = -1,
+    alias = '',
+    subscribe = false,
+    smartFilter = false,
+    enabled = true,
+  ): Promise<RssUpdateResponse> {
+    const params = new URLSearchParams();
+    params.set('url', url);
+    params.set('alias', alias);
+    params.set('feed-id', id.toString());
+    params.set('subscribe', Number(subscribe).toString());
+    params.set('smart-filter', Number(smartFilter).toString());
+    params.set('enabled', JSON.stringify(enabled));
+    const res = await this.request<RssUpdateResponse>('rss-update', params);
+    return res.body;
+  }
+
+  async rssRemove(id: number): Promise<BaseResponse> {
+    const params = new URLSearchParams();
+    params.set('feedid', id.toString());
+    const res = await this.request<BaseResponse>('rss-remove', params);
+    return res.body;
+  }
+
+  async listTorrents(): Promise<TorrentListResponse> {
+    const params = new URLSearchParams();
+    params.set('list', '1');
+    const res = await this.request<TorrentListResponse>('', params);
+    return res.body;
+  }
+
+  async connect(): Promise<void> {
+    const url = urlJoin(this.config.baseUrl, this.config.path, '/token.html');
+
+    const headers = {
+      Authorization: this._authorization(),
+    };
+    const params = new URLSearchParams();
+    params.set('t', Date.now().toString());
+    const options: OptionsOfTextResponseBody = {
+      headers,
+      searchParams: params,
+      retry: { limit: 0 },
+      responseType: 'text',
+    };
+    if (this.config.timeout) {
+      options.timeout = { request: this.config.timeout };
+    }
+
+    if (this.config.agent) {
+      options.agent = this.config.agent;
+    }
+
+    const res = await got.get(url, options);
+    this._cookie = Cookie.parse(res.headers?.['set-cookie']?.[0] ?? '');
+    // example token response
+    // <html><div id='token' style='display:none;'>gBPEW_SyrgB-RSmF3tZvqSsK9Ht7jk4uAAAAAC61XoYAAAAATyqNE_uq8lwAAAAA</div></html>
+    const regex = />([^<]+)</;
+    const match = regex.exec(res.body);
+    if (match) {
+      this._token = match[match.length - 1];
+      return;
+    }
+
+    throw new Error('Valid token not found');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  async request<T extends object>(
+    action: string,
+    params: URLSearchParams = new URLSearchParams(),
+  ): Promise<Response<T>> {
+    if (this._cookie) {
+      // eslint-disable-next-line new-cap
+      if (this._cookie.TTL() < 5000) {
+        this.resetSession();
+      }
+    }
+
+    if (!this._token) {
+      await this.connect();
+    }
+
+    params.set('token', this._token!);
+    params.set('t', Date.now().toString());
+    // allows action to be an empty string
+    if (action) {
+      params.set('action', action);
+    }
+
+    const url = urlJoin(this.config.baseUrl, this.config.path);
+    return got.get<T>(url, {
+      headers: {
+        Authorization: this._authorization(),
+        Cookie: this._cookie?.cookieString(),
+      },
+      searchParams: params,
+      retry: { limit: 0 },
+      timeout: { request: this.config.timeout },
+      ...(this.config.agent ? { agent: this.config.agent } : {}),
+      responseType: 'json',
+    });
+  }
+
+  private _authorization(): string {
+    const str = `${this.config.username ?? ''}:${this.config.password ?? ''}`;
+    const encoded = Buffer.from(str).toString('base64');
+    return 'Basic ' + encoded;
+  }
+
+  private _normalizeTorrentData(torrent: TorrentData): NormalizedTorrent {
+    const torrentState: number = torrent[1];
+    const progress: number = torrent[4] / 100;
+    const done = progress >= 100;
+    const isCompleted = progress >= 100;
+
+    // TODO: Convert from bitwise
+
+    let state = TorrentState.unknown;
+    // eslint-disable-next-line no-bitwise
+    if (torrentState & STATE_PAUSED) {
+      // paused
+      state = TorrentState.paused;
+      // eslint-disable-next-line no-bitwise
+    } else if (torrentState & STATE_STARTED) {
+      // started, seeding or leeching
+      if (done) {
+        state = TorrentState.seeding;
+      } else {
+        state = TorrentState.downloading;
+      }
+      // if (!(torrentState & STATE_QUEUED)) {
+      //   // forced start
+      //   res[1] = '[F] ' + res[1];
+      // }
+      // eslint-disable-next-line no-bitwise
+    } else if (torrentState & STATE_CHECKING) {
+      // checking
+      state = TorrentState.checking;
+      // eslint-disable-next-line no-bitwise
+    } else if (torrentState & STATE_ERROR) {
+      // error
+      state = TorrentState.error;
+      // eslint-disable-next-line no-bitwise
+    } else if (torrentState & STATE_QUEUED) {
+      // queued
+      state = TorrentState.queued;
+    } else if (done) {
+      // finished
+      state = TorrentState.paused;
+    } else {
+      // stopped
+      state = TorrentState.paused;
+    }
+
+    const result: NormalizedTorrent = {
+      id: torrent[0].toLowerCase(),
+      name: torrent[2],
+      state,
+      isCompleted,
+      stateMessage: '',
+      progress,
+      ratio: torrent[7] / 1000,
+      dateAdded: new Date(torrent[23] * 1000).toISOString(),
+      dateCompleted: new Date(torrent[24] * 1000).toISOString(),
+      label: torrent[11],
+      savePath: torrent[26],
+      uploadSpeed: torrent[8],
+      downloadSpeed: torrent[9],
+      eta: torrent[10],
+      queuePosition: torrent[17],
+      connectedPeers: torrent[12],
+      connectedSeeds: torrent[14],
+      totalPeers: torrent[13],
+      totalSeeds: torrent[15],
+      totalSelected: torrent[18],
+      totalSize: torrent[3],
+      totalUploaded: torrent[6],
+      totalDownloaded: torrent[5],
+    };
+    return result;
+  }
+}
+
+const STATE_STARTED = 1;
+const STATE_CHECKING = 2;
+const STATE_ERROR = 16;
+const STATE_PAUSED = 32;
+const STATE_QUEUED = 64;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,130 +1,170 @@
-import fs from 'fs';
-import path from 'path';
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
 
-import { afterEach, describe, expect, it } from '@jest/globals';
+import test, { ExecutionContext } from 'ava';
 
-import { Utorrent } from '../src';
+import { Utorrent } from '../src/index.js';
 
-const baseUrl = process.env.BASE_URL ?? 'http://localhost:8080/';
+const baseUrl = process.env['BASE_URL'] ?? 'http://localhost:8080/';
 const torrentName = 'ubuntu-18.04.1-desktop-amd64.iso';
-const torrentFile = path.join(__dirname, '/ubuntu-18.04.1-desktop-amd64.iso.torrent');
+const dirname = new URL('.', import.meta.url).pathname;
+const torrentFile = path.join(dirname, '/ubuntu-18.04.1-desktop-amd64.iso.torrent');
+const magnet =
+  'magnet:?xt=urn:btih:B0B81206633C42874173D22E564D293DAEFC45E2&dn=Ubuntu+11+10+Alternate+Amd64+Iso&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.open-internet.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.si%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Fdenis.stalker.upeer.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce';
 
-async function setupTorrent(client: Utorrent): Promise<string> {
+async function setupTorrent(client: Utorrent, t: ExecutionContext): Promise<string> {
   await client.addTorrent(torrentFile);
   const res = await client.listTorrents();
-  expect(res.torrents).toHaveLength(1);
-  return res.torrents[0][0];
+  t.is(res.torrents.length, 1);
+  return res.torrents[0]![0];
 }
 
-describe('Ubuntu', () => {
-  afterEach(async () => {
-    const client = new Utorrent({ baseUrl });
-    const res = await client.listTorrents();
-    for (const torrent of res.torrents) {
-      // clean up all torrents
-      // eslint-disable-next-line no-await-in-loop
-      await client.removeTorrent(torrent[0]);
-    }
-  });
-  it('should be instantiable', () => {
-    const client = new Utorrent({ baseUrl });
-    expect(client).toBeTruthy();
-  });
-  it('should connect', async () => {
-    const client = new Utorrent({ baseUrl });
-    await client.connect();
-    expect((client as any)._token.length).toBeGreaterThan(0);
-  });
-  it('should disconnect', async () => {
-    const client = new Utorrent({ baseUrl });
-    await client.connect();
-    client.resetSession();
-    expect((client as any)._token).toBeUndefined();
-  });
-  it('should add torrent', async () => {
-    const client = new Utorrent({ baseUrl });
-    await client.addTorrent(fs.readFileSync(torrentFile));
-    const res = await client.listTorrents();
-    expect(res.torrents).toHaveLength(1);
-  });
-  it('should get settings', async () => {
-    const client = new Utorrent({ baseUrl });
-    const res = await client.getSettings();
-    expect(res.settings).toBeInstanceOf(Array);
-  });
-  it('should list torrents', async () => {
-    const client = new Utorrent({ baseUrl });
-    await setupTorrent(client);
-    const res = await client.listTorrents();
-    expect(res.torrents).toHaveLength(1);
-    expect(res.torrents[0][2]).toBe(torrentName);
-  });
-  it('should move torrents in queue', async () => {
-    const client = new Utorrent({ baseUrl });
-    const key = await setupTorrent(client);
-    await client.queueUp(key);
-    await client.queueDown(key);
-    await client.queueTop(key);
-    await client.queueBottom(key);
-  });
-  it('should remove torrent', async () => {
-    const client = new Utorrent({ baseUrl });
-    const key = await setupTorrent(client);
-    await client.removeTorrent(key);
-    const res = await client.listTorrents();
-    expect(res.torrents).toHaveLength(0);
-  });
-  it('should return normalized torrent data', async () => {
-    const client = new Utorrent({ baseUrl });
-    await setupTorrent(client);
-    const res = await client.getAllData();
-    const torrent = res.torrents[0];
-    expect(torrent.connectedPeers).toBe(0);
-    expect(torrent.connectedSeeds).toBe(0);
-    expect(torrent.downloadSpeed).toBe(0);
-    // expect(torrent.eta).toBe(0);
-    expect(torrent.isCompleted).toBe(false);
-    expect(torrent.label).toBe('');
-    expect(torrent.name).toBe(torrentName);
-    expect(torrent.progress).toBeGreaterThanOrEqual(0);
-    expect(torrent.queuePosition).toBe(1);
-    expect(torrent.ratio).toBe(0);
-    // expect(torrent.savePath).toBe('/utorrent/data/incomplete');
-    // expect(torrent.state).toBe(TorrentState.queued);
-    expect(torrent.stateMessage).toBe('');
-    expect(torrent.totalDownloaded).toBe(0);
-    expect(torrent.totalPeers).toBe(0);
-    expect(torrent.totalSeeds).toBe(0);
-    // expect(torrent.totalSelected).toBe(1953349632);
-    // expect(torrent.totalSize).toBe(1953349632);
-    expect(torrent.totalUploaded).toBe(0);
-    expect(torrent.uploadSpeed).toBe(0);
-  });
-  it('should add torrent with normalized response', async () => {
-    const client = new Utorrent({ baseUrl });
+test.afterEach(async () => {
+  const client = new Utorrent({ baseUrl });
+  const res = await client.listTorrents();
+  for (const torrent of res.torrents) {
+    // clean up all torrents
+    // eslint-disable-next-line no-await-in-loop
+    await client.removeTorrent(torrent[0]);
+  }
+});
+test('should be instantiable', t => {
+  const client = new Utorrent({ baseUrl });
+  t.truthy(client);
+});
+test.serial('should connect', async t => {
+  const client = new Utorrent({ baseUrl });
+  await client.connect();
+  t.assert((client as any)._token.length > 0);
+});
+test.serial('should disconnect', async t => {
+  const client = new Utorrent({ baseUrl });
+  await client.connect();
+  client.resetSession();
+  t.is((client as any)._token, undefined);
+});
+test.serial('should add torrent from buffer', async t => {
+  const client = new Utorrent({ baseUrl });
+  await client.addTorrent(readFileSync(torrentFile));
+  const res = await client.listTorrents();
+  t.is(res.torrents.length, 1);
+});
+test.serial('should add torrent from path', async t => {
+  const client = new Utorrent({ baseUrl });
+  await client.addTorrent(torrentFile);
+  const res = await client.listTorrents();
+  t.is(res.torrents.length, 1);
+});
+test.serial('should add torrent from string', async t => {
+  const client = new Utorrent({ baseUrl });
+  await client.addTorrent(readFileSync(torrentFile).toString('base64'));
+  const res = await client.listTorrents();
+  t.is(res.torrents.length, 1);
+});
+test.serial('should get settings', async t => {
+  const client = new Utorrent({ baseUrl });
+  const res = await client.getSettings();
+  t.assert(res.settings instanceof Array);
+});
+test.serial('should list torrents', async t => {
+  const client = new Utorrent({ baseUrl });
+  await setupTorrent(client, t);
+  const res = await client.listTorrents();
+  t.is(res.torrents.length, 1);
+  t.is(res.torrents[0]?.[2], torrentName);
+});
+test.serial('should move torrents in queue', async t => {
+  const client = new Utorrent({ baseUrl });
+  const key = await setupTorrent(client, t);
+  await client.queueUp(key);
+  await client.queueDown(key);
+  await client.queueTop(key);
+  await client.queueBottom(key);
+});
+test.serial('should remove torrent', async t => {
+  const client = new Utorrent({ baseUrl });
+  const key = await setupTorrent(client, t);
+  await client.removeTorrent(key);
+  const res = await client.listTorrents();
+  t.is(res.torrents.length, 0);
+});
+test.serial('should return normalized torrent data', async t => {
+  const client = new Utorrent({ baseUrl });
+  await setupTorrent(client, t);
+  const res = await client.getAllData();
+  const torrent = res.torrents[0]!;
+  t.is(torrent.connectedPeers, 0);
+  t.is(torrent.connectedSeeds, 0);
+  t.is(torrent.downloadSpeed, 0);
+  // t.is(torrent.eta, 0);
+  t.is(torrent.isCompleted, false);
+  t.is(torrent.label, '');
+  t.is(torrent.name, torrentName);
+  t.assert(torrent.progress >= 0);
+  t.is(torrent.queuePosition, 1);
+  t.is(torrent.ratio, 0);
+  // t.is(torrent.savePath, '/utorrent/data/incomplete');
+  // t.is(torrent.state, TorrentState.queued);
+  t.is(torrent.stateMessage, '');
+  t.is(torrent.totalDownloaded, 0);
+  t.is(torrent.totalPeers, 0);
+  t.is(torrent.totalSeeds, 0);
+  // t.is(torrent.totalSelected, 1953349632);
+  // t.is(torrent.totalSize, 1953349632);
+  t.is(torrent.totalUploaded, 0);
+  t.is(torrent.uploadSpeed, 0);
+});
+test.serial('should add torrent with normalized response', async t => {
+  const client = new Utorrent({ baseUrl });
 
-    const torrent = await client.normalizedAddTorrent(fs.readFileSync(torrentFile), {
-      label: 'test',
-    });
-    expect(torrent.connectedPeers).toBe(0);
-    expect(torrent.connectedSeeds).toBe(0);
-    expect(torrent.downloadSpeed).toBe(0);
-    // expect(torrent.eta).toBe(0);
-    expect(torrent.isCompleted).toBe(false);
-    expect(torrent.label).toBe('test');
-    expect(torrent.name).toBe(torrentName);
-    expect(torrent.progress).toBeGreaterThanOrEqual(0);
-    expect(torrent.queuePosition).toBe(1);
-    expect(torrent.ratio).toBe(0);
-    // expect(torrent.savePath).toBe('/utorrent/data/incomplete');
-    // expect(torrent.state).toBe(TorrentState.queued);
-    expect(torrent.stateMessage).toBe('');
-    expect(torrent.totalDownloaded).toBe(0);
-    expect(torrent.totalPeers).toBe(0);
-    expect(torrent.totalSeeds).toBe(0);
-    // expect(torrent.totalSelected).toBe(1953349632);
-    // expect(torrent.totalSize).toBe(1953349632);
-    expect(torrent.totalUploaded).toBe(0);
-    expect(torrent.uploadSpeed).toBe(0);
+  const torrent = await client.normalizedAddTorrent(readFileSync(torrentFile), {
+    label: 'test',
   });
+  t.is(torrent.connectedPeers, 0);
+  t.is(torrent.connectedSeeds, 0);
+  t.is(torrent.downloadSpeed, 0);
+  // t.is(torrent.eta, 0);
+  t.is(torrent.isCompleted, false);
+  t.is(torrent.label, 'test');
+  t.is(torrent.name, torrentName);
+  t.assert(torrent.progress >= 0);
+  t.is(torrent.queuePosition, 1);
+  t.is(torrent.ratio, 0);
+  // t.is(torrent.savePath, '/utorrent/data/incomplete');
+  // t.is(torrent.state, TorrentState.queued);
+  t.is(torrent.stateMessage, '');
+  t.is(torrent.totalDownloaded, 0);
+  t.is(torrent.totalPeers, 0);
+  t.is(torrent.totalSeeds, 0);
+  // t.is(torrent.totalSelected, 1953349632);
+  // t.is(torrent.totalSize, 1953349632);
+  t.is(torrent.totalUploaded, 0);
+  t.is(torrent.uploadSpeed, 0);
+});
+test.serial('should add torrent with normalized response from magnet', async t => {
+  const client = new Utorrent({ baseUrl });
+
+  const torrent = await client.normalizedAddTorrent(magnet, {
+    label: 'test',
+  });
+  t.is(torrent.connectedPeers, 0);
+  t.is(torrent.connectedSeeds, 0);
+  t.is(torrent.downloadSpeed, 0);
+  // t.is(torrent.eta, 0);
+  t.is(torrent.isCompleted, false);
+  t.is(torrent.label, 'test');
+  t.is(torrent.name, 'Ubuntu 11 10 Alternate Amd64 Iso');
+  t.assert(torrent.progress >= 0);
+  t.is(torrent.queuePosition, 1);
+  t.is(torrent.ratio, 0);
+  // t.is(torrent.savePath, '/utorrent/data/incomplete');
+  // t.is(torrent.state, TorrentState.queued);
+  t.is(torrent.stateMessage, '');
+  t.is(torrent.totalDownloaded, 0);
+  t.is(torrent.totalPeers, 0);
+  t.is(torrent.totalSeeds, 0);
+  // t.is(torrent.totalSelected, 1953349632);
+  // t.is(torrent.totalSize, 1953349632);
+  t.is(torrent.totalUploaded, 0);
+  t.is(torrent.uploadSpeed, 0);
 });
